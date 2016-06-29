630187ea501d815d913d7e9d80670e7d989dea89
Marcel de Graaf <mail@marceldegraaf.net>
2016-06-29 19:15:58 +0200

    test cas strategy

--- test/ueberauth/strategy/cas_test.exs
+++ test/ueberauth/strategy/cas_test.exs
@@ -1,7 +1,18 @@
-defmodule Ueberauth.Strategy.CASTest do
+defmodule Ueberauth.Strategy.CAS.Test do
   use ExUnit.Case
 
   alias Ueberauth.Strategy.CAS
+
+  setup do
+    conn = %Plug.Conn{
+      private: %{
+        cas_user: %CAS.User{name: "Marcel de Graaf", email: "mail@marceldegraaf.net", roles: ["developer"]},
+        cas_ticket: "ST-XXXXX",
+      }
+    }
+
+    {:ok, conn: conn}
+  end
 
   test "login callback without service ticket shows an error" do
     conn = CAS.handle_callback!(%Plug.Conn{params: %{}})
@@ -15,16 +26,37 @@ defmodule Ueberauth.Strategy.CASTest do
     assert conn.private.cas_user != nil
   end
 
-  test "cleanup callback" do
-    conn = CAS.handle_cleanup!(%Plug.Conn{private: %{cas_ticket: "ST-XXXXX", cas_user: "a user"}})
+  test "cleanup callback", %{conn: conn} do
+    conn = CAS.handle_cleanup!(conn)
 
     assert conn.private.cas_user == nil
     assert conn.private.cas_ticket == nil
   end
 
-  test "use user email as uniqe uid" do
-    uid = CAS.uid(%Plug.Conn{private: %{cas_user: %CAS.User{name: "Marcel de Graaf", email: "mail@marceldegraaf.net"}}})
+  test "use user email as uniqe uid", %{conn: conn} do
+    uid = CAS.uid(conn)
 
     assert uid == "mail@marceldegraaf.net"
+  end
+
+  test "generates info struct", %{conn: conn} do
+    info = CAS.info(conn)
+
+    assert info.name == "Marcel de Graaf"
+    assert info.email == "mail@marceldegraaf.net"
+  end
+
+  test "generates credentials struct", %{conn: conn} do
+    credentials = CAS.credentials(conn)
+
+    assert credentials.expires == false
+    assert credentials.token == "ST-XXXXX"
+    assert credentials.other == ["developer"]
+  end
+
+  test "generates extra struct", %{conn: conn} do
+    extra = CAS.extra(conn)
+
+    assert extra.raw_info.user == conn.private.cas_user
   end
 end
