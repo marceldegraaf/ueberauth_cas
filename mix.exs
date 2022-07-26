6e2c28c45c42e922fb19592c0b2a3d6f93174bb1
Niko Strijbol <strijbol.niko@gmail.com>
2022-07-26 23:28:54 +0200

    Bump to v2.3.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule UeberauthCAS.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/marceldegraaf/ueberauth_cas"
-  @version "2.2.0"
+  @version "2.3.0"
 
   def project do
     [
